75425f391ebb808a0a36482d09b4acea3ac6c665
wayne <qleonnex@gmail.com>
2026-01-08 01:25:32 +0500

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-import vercelServerless from "@astrojs/vercel"
+import vercelServerless from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
